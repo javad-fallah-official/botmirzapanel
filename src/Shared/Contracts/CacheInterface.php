20e8c1a053b6a4d95ddd6c7f5e5505aacb57f4fd
(Unknown)jack <142024753+javad-fallah-official@users.noreply.github.com>
2025-08-29 23:30:38 +0330

    feat: add CacheInterface contract with PSR-16 compliance

---
+++ src/Shared/Contracts/CacheInterface.php
@@ -0,0 +1,178 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Shared\Contracts;
+
+/**
+ * Cache Interface
+ * 
+ * Defines the contract for cache implementations providing
+ * standardized caching operations across the application.
+ * 
+ * This interface follows PSR-16 Simple Cache Interface standards
+ * with additional features for advanced caching scenarios.
+ */
+interface CacheInterface
+{
+    /**
+     * Fetches a value from the cache.
+     *
+     * @param string $key The unique key of this item in the cache.
+     * @param mixed $default Default value to return if the key does not exist.
+     *
+     * @return mixed The value of the item from the cache, or $default in case of cache miss.
+     */
+    public function get(string $key, mixed $default = null): mixed;
+
+    /**
+     * Persists data in the cache, uniquely referenced by a key with an optional expiration TTL time.
+     *
+     * @param string $key The key of the item to store.
+     * @param mixed $value The value of the item to store, must be serializable.
+     * @param null|int $ttl Optional. The TTL value of this item. If no value is sent and
+     *                      the driver supports TTL then the library may set a default value
+     *                      for it or let the driver take care of that.
+     *
+     * @return bool True on success and false on failure.
+     */
+    public function set(string $key, mixed $value, ?int $ttl = null): bool;
+
+    /**
+     * Delete an item from the cache by its unique key.
+     *
+     * @param string $key The unique cache key of the item to delete.
+     *
+     * @return bool True if the item was successfully removed. False if there was an error.
+     */
+    public function delete(string $key): bool;
+
+    /**
+     * Wipes clean the entire cache's keys.
+     *
+     * @return bool True on success and false on failure.
+     */
+    public function clear(): bool;
+
+    /**
+     * Obtains multiple cache items by their unique keys.
+     *
+     * @param array $keys A list of keys that can obtained in a single operation.
+     * @param mixed $default Default value to return for keys that do not exist.
+     *
+     * @return array A list of key => value pairs. Cache keys that do not exist or are stale will have $default as value.
+     */
+    public function getMultiple(array $keys, mixed $default = null): array;
+
+    /**
+     * Persists a set of key => value pairs in the cache, with an optional TTL.
+     *
+     * @param array $values A list of key => value pairs for a multiple-set operation.
+     * @param null|int $ttl Optional. The TTL value of this item. If no value is sent and
+     *                       the driver supports TTL then the library may set a default value
+     *                       for it or let the driver take care of that.
+     *
+     * @return bool True on success and false on failure.
+     */
+    public function setMultiple(array $values, ?int $ttl = null): bool;
+
+    /**
+     * Deletes multiple cache items in a single operation.
+     *
+     * @param array $keys A list of string-based keys to be deleted.
+     *
+     * @return bool True if the items were successfully removed. False if there was an error.
+     */
+    public function deleteMultiple(array $keys): bool;
+
+    /**
+     * Determines whether an item is present in the cache.
+     *
+     * NOTE: It is recommended that has() is only to be used for cache warming type purposes
+     * and not to be used within your live applications operations for get/set, as this method
+     * is subject to a race condition where your has() will return true and immediately after,
+     * another script can remove it making the state of your app out of date.
+     *
+     * @param string $key The cache item key.
+     *
+     * @return bool
+     */
+    public function has(string $key): bool;
+
+    /**
+     * Get an item from the cache, or execute the given Closure and store the result.
+     *
+     * @param string $key The cache key.
+     * @param callable $callback Callback to execute if cache miss occurs.
+     * @param null|int $ttl Optional TTL for the cached value.
+     *
+     * @return mixed
+     */
+    public function remember(string $key, callable $callback, ?int $ttl = null): mixed;
+
+    /**
+     * Get an item from the cache, or execute the given Closure and store the result forever.
+     *
+     * @param string $key The cache key.
+     * @param callable $callback Callback to execute if cache miss occurs.
+     *
+     * @return mixed
+     */
+    public function rememberForever(string $key, callable $callback): mixed;
+
+    /**
+     * Increment the value of an item in the cache.
+     *
+     * @param string $key The cache key.
+     * @param int $value The value to increment by.
+     *
+     * @return int|false The new value on success, or false on failure.
+     */
+    public function increment(string $key, int $value = 1): int|false;
+
+    /**
+     * Decrement the value of an item in the cache.
+     *
+     * @param string $key The cache key.
+     * @param int $value The value to decrement by.
+     *
+     * @return int|false The new value on success, or false on failure.
+     */
+    public function decrement(string $key, int $value = 1): int|false;
+
+    /**
+     * Store an item in the cache if the key doesn't exist.
+     *
+     * @param string $key The cache key.
+     * @param mixed $value The value to store.
+     * @param null|int $ttl Optional TTL for the cached value.
+     *
+     * @return bool True if the item was stored, false otherwise.
+     */
+    public function add(string $key, mixed $value, ?int $ttl = null): bool;
+
+    /**
+     * Get cache statistics.
+     *
+     * @return array Array containing cache statistics.
+     */
+    public function getStats(): array;
+
+    /**
+     * Remove all cache entries associated with the given tags.
+     *
+     * @param array $tags Array of tags to flush.
+     *
+     * @return bool True on success, false on failure.
+     */
+    public function flushTags(array $tags): bool;
+
+    /**
+     * Warm up the cache with predefined data.
+     *
+     * @param array $data Array of cache data to preload.
+     *
+     * @return bool True on success, false on failure.
+     */
+    public function warmUp(array $data): bool;
+}
